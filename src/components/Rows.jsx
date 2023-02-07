e365aaad296125b98f4f723053a998fd686cd77c
Lakan Santos <rizesuoracle25@gmail.com>
2023-02-07 20:00:31 +0800

    yehey, i was able to use useRef in button

--- src/components/Rows.jsx
+++ src/components/Rows.jsx
@@ -1,5 +1,5 @@
 
-import React, { useCallback, useState, useEffect } from "react";
+import React, { useCallback, useState, useEffect, useRef} from "react";
 import SkeletonTemplate from "./SkeletonTemplate";
 
 const Rows = ({title, fetchURL}) => {
@@ -17,14 +17,20 @@ const Rows = ({title, fetchURL}) => {
     useEffect(() => {
 
         fetchData()
-        
+       
     }, [fetchData])
     
-    
-        return data?(
+    const containerRef = useRef(null);
+    useEffect(() => {
+        if(containerRef.current){
+            containerRef.current.scrollLeft += 750;
+        }
+    }, [containerRef])
+
+        return data ? (
             <div className="Rows h-full w-full py-10">
                 <h1 className="text-white text-4xl my-4">{title}</h1>
-                <div className="img-container h-fit w-full flex flex-row gap-5 overflow-auto">
+                <div className="img-container h-fit w-full flex flex-row gap-5 overflow-auto" ref={containerRef} style={{scrollBehavior: 'smooth',}}>
                 {data.results.map(result => (
                     <div className="w-full h-full hover:scale-110 duration-700" key={result.id}>
                         <img src={result.image} alt="" className="w-64 h-96 hover:cursor-pointer "/>
@@ -34,6 +40,24 @@ const Rows = ({title, fetchURL}) => {
                     </div>                      
                 ))};
                 </div>
+                <button 
+                className="mt-5 text-white bg-gray-800 px-5 py-2 rounded"
+                onClick={() => {
+                    if(containerRef.current){
+                        containerRef.current.scrollLeft -= 750;
+                    }       
+                }}>
+                    Scroll Left
+                </button>
+                <button 
+                className="mt-5 text-white bg-gray-800 px-5 py-2 rounded"
+                onClick={() => {
+                    if(containerRef.current){
+                        containerRef.current.scrollLeft += 750;
+                    }       
+                }}>
+                    Scroll Right
+                </button>
             </div>
         ) : (
             <div className="Rows h-96 w-full mt-20  flex flex-row gap-5">
@@ -42,6 +66,7 @@ const Rows = ({title, fetchURL}) => {
         )
 
 }
+
 
 
 export default Rows;
