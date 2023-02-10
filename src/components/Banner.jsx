883e0b0dd68e0a2cf07f5fdb6e9587a057038b2f
Lakan Santos <rizesuoracle25@gmail.com>
2023-02-10 15:44:19 +0800

    testing fixing responsive issue of blur banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -52,9 +52,10 @@ const Banner = () => {
         return () => clearInterval(intervalId)
     }, [currentIndex])
 
+
     return data && resultsData.length > 0 ?(
         <div 
-        className="banner h-[60vh] sm:h-[70vh] flex flex-col  justify-between" 
+        className="banner h-[70vh] sm:h-[70vh] flex flex-col  justify-between" 
         style={{
             background: `url(${resultsData[currentIndex] ? resultsData[currentIndex].cover :null}) center/cover no-repeat rgba(0,0,0, .8)`,
             backgroundBlendMode: 'multiply'
