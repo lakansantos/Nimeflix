79ff3ff6f1f4d225bf5df097e83e94835546640a
Lakan Santos <rizesuoracle25@gmail.com>
2023-02-10 18:59:22 +0800

    changed image directory

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ const Navbar = () => {
 
     return (
         <nav className="h-fit w-full flex flex-row justify-between fixed z-[100] bg-black/70">
-            <img src="src/img/Nimeflix logo.png" alt="Nimeflix"  className="p-2 ml-4 sm:ml-5 hover:cursor-pointer  h-[60px] w-[60px] sm:h-[70px] sm:w-[70px]"/>
+            <a href=""><img src="public/Nimeflix logo.png" alt="Nimeflix"  className="p-2 ml-4 sm:ml-5 hover:cursor-pointer  h-[60px] w-[60px] sm:h-[70px] sm:w-[70px]"/></a>
             <ul className="flex flex-row justify-evenly items-center w-1/4 sm:w-[10%] mr-3 sm:mr-0">
                 <li><AiOutlineSearch fontSize={'2.5rem'} color={'white'}/></li>
-                <li><img src="src/img/avatar.png" alt="avatar-icon" className="w-60px h-[55px]"/></li>
+                <li><img src="public/avatar.png" alt="avatar-icon" className="w-60px h-[55px]"/></li>
             </ul>
         </nav>
     )
