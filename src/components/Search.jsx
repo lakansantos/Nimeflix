b31cd1d57670296f104d3f2432a548bd0298fb0f
Lakan Santos <rizesuoracle25@gmail.com>
2023-02-17 15:19:01 +0800

    added search feature

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,40 +5,100 @@ import { useState, useCallback, useEffect } from 'react';
 const Search = ({showInputSearch, setShowInputSearch}) => { 
 
     const [allData, setAllData] = useState(null)
-
-    const fetchData = useCallback(() => {
+    const [allResultsData, setAllResultsData] = useState([])
+    const [showSearchResults, setShowSearchResults] = useState(false)
+    const [query, setQuery] = useState('')
+    const [filteredData, setFilteredData] = useState([])
+    const [makeSet, setSet] = useState([])
+ 
+    const fetchAllData = useCallback(() => {
         Promise.all([
-            fetch(requests.popularData),
             fetch(requests.trendingData),
-            fetch(requests.recentlyAdded)
+            fetch(requests.popularData),
+            fetch(requests.recentlyAdded),
         ])
-        .then(responses => Promise.all(responses.map((response => response.json()))))
-        .then(data => setAllData(data))
-        .catch(err => console.log(err));
 
+        .then((responses => Promise.all(responses.map(response => response.json()))))
+        .then((data => {
+            setAllData(data)
+            setAllResultsData([...new Set(data.map(item => item.results).flat())])
+        }))
+        .catch((err => console.log(err)))
     }, [])
+    
+    useEffect(() => {
+        fetchAllData();
+    }, [fetchAllData])
+    
+    
+    const handleSearch = (e) => {
+        setQuery(e.target.value)    
+    }
 
     useEffect(() => {
-        fetchData();
-    }, [fetchData])
+        if(query){
+            setShowSearchResults(true)
+        } else{
+            setShowSearchResults(false)
+        }
+    }, [query])
+
+   
+
+    useEffect(() => {
+        if(allResultsData){
+            let filteringData = allResultsData.filter((item => {
+
+                const englishTitle = item.title.english || '';
+                const romajiTitle = item.title.romaji || '';
+                const nativeTitle = item.title.native || '';
+                return (
+                    englishTitle.toLowerCase().includes(query.toLowerCase()) ||
+                    romajiTitle.toLowerCase().includes(query.toLowerCase()) ||
+                    nativeTitle.toLowerCase().includes(query.toLowerCase())
+                ) 
+            }))
     
-    console.log(allData)
-    
+            setFilteredData(filteringData)
+        }
+    }, [query, allResultsData])
+
     return(
-        <div className={"flex items-center justify-end relative overflow-hidden w-[100%] h-1/2"}>
-        <input type="text"
-        placeholder="Search a movie, a title, genre..."
-        className={!showInputSearch ? "opacity-0" : "opacity-1 w-full  h-[60px] indent-2"} 
-        style={showInputSearch ? {animation: '.4s linear showInput'} : {}}/>
-        <li>
-            <AiOutlineSearch 
-            fontSize={'2.5rem'} color={'white'} 
-            className={`hover:cursor-pointer w-[60px] h-[30px] ${!showInputSearch ? '' : 'bg-lightBlue'}`} 
-            onClick={() => setShowInputSearch(!showInputSearch)}
-            style={showInputSearch ? {position: 'absolute', top: 0, right: 0, width: '60px', height: '100%', animation: '.1s linear movingSearch'}: {}}
-            />
-        </li>
-    </div>
+        <>
+        <div className={`flex items-center justify-end relative  w-[100%] h-1/2 ${showSearchResults ? '' : 'overflow-hidden'}`}
+        >
+            <input type="text"
+            placeholder="Search a movie, title, genre..."
+            className={!showInputSearch ? "opacity-0" : "opacity-1 w-full  h-full indent-2"} 
+            style={showInputSearch ? {animation: '.4s linear showInput'} : {}}
+            onChange={(e) => handleSearch(e)}
+           />
+            <li>
+                <AiOutlineSearch 
+                fontSize={'2.5rem'} color={'white'} 
+                className={`hover:cursor-pointer w-[60px] h-[30px] ${!showInputSearch ? '' : 'bg-lightBlue'}`} 
+                onClick={() => setShowInputSearch(!showInputSearch)}
+                style={showInputSearch ? {position: 'absolute', top: 0, right: 0, width: '60px', height: '100%', animation: '.1s linear movingSearch'}: {}}
+                />
+            </li>
+            {showSearchResults && filteredData ?
+                  
+                    <div style={{position: 'absolute', top: '100%'}}
+                    className="text-white border border-solid border-lightBlue w-full min-h-[100px] max-h-[500px] overflow-auto bg-black">
+                        {filteredData.map((data, index) => (
+                        <div key={index} className='flex justify-center items-center my-2'>
+                            <div className='w-full flex h-[50px] justify-start items-center'>
+                                <img src={data.image} alt="title image" className='w-[50px] h-full mr-5'/>
+                                {data.title.english ? data.title.english.slice(0, 50) + '...' : data.title.romaji ? data.title.romaji :  data.title.native}
+                            </div>
+                        </div>
+        ))}
+                    </div>
+                   : null
+            }      
+        </div>
+        
+        </>
     )
 
 }
